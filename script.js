ae3e45135793b401e1edbcad5d40941d7a5b0f01
appert <scoreur@gmail.com>
2022-11-19 10:45:04 +0100

    rajout scrollbar stylisé, et typewriter

--- script.js
+++ script.js
@@ -1,4 +1,50 @@
-                        /*----------------------- Menu, Scroll, dark/light ----------------------*/
+ 
+ const textAnim = document.querySelector("h6");
+
+ new Typewriter (textAnim, {
+     deleteSpeed: 40
+ })
+ .changeDelay(50)
+ .typeString('Web Developer')
+ .pauseFor(300)
+ .typeString(', Dev full stack !')
+ .pause(1000)
+ .deleteChars(13)
+ .typeString('<span style="color: #D84923"> HTML!')
+ .pause(1000)
+ .deleteChars(6)
+ .typeString('<span style="color: #2091EB"> CSS !')
+ .pause(1000)
+ .deleteChars(5)
+ .typeString('<span style="color: #3EAF7C"> Vue !')
+ .pause(1000)
+ .deleteChars(5)
+ .typeString('<span style="color: #89BB3C"> Node !')
+ .pause(1000)
+ .deleteChars(6)
+ .typeString('<span style="color: #EAD41C"> JavaScript !')
+ .pause(1000)
+ .deleteChars(12)
+ .typeString('<span style="color: #E3A652"> MySQL, MongoDb !')
+ .pause(1000)
+ .deleteChars(16)
+ .typeString('<span style="color: #f7043d"> Pentest, Osint !')
+ .pause(1000)
+ .deleteChars(16)
+ .typeString('<span style="color: #2091EB"> Application M.E.V.N !')
+ .pause(1000)
+ .deleteChars(21)
+ .typeString('<span style="color: #3586ff"> Application S.E.V.N !')
+ .pause(1000)
+ .deleteChars(21)
+ .typeString('<span style="color: #d60c3e"> Full Stack !')
+ .start()
+ 
+ 
+ 
+ 
+ 
+ /*----------------------- Menu, Scroll, dark/light ----------------------*/
 
 let menu = document.querySelector('#menu-btn');
 let header = document.querySelector('.header');
